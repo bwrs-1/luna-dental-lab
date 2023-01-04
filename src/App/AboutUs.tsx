aae741c055bf6f051c3d71647cafcf35f834cc05
bwrs-1 <taisei.nagano@bwrs.page>
2023-01-04 15:47:35 +0900

    メモ追加画面 一時コメントアウト

--- src/App/AboutUs.tsx
+++ src/App/AboutUs.tsx
@@ -6,7 +6,7 @@ const Content = () => {
     <div className="about-us">
       <div className="container">
         <h2>各クリニック メモ帳</h2>
-        <iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQIZDLRQ29Pjo-sULvwnhdMq6kxcsx7hUHFliu9jg7_v1B5eCd-N6ctuUWfoOrTDS-ijfh75S3HJKmy/pubhtml?gid=1759418424&amp;single=true&amp;widget=true&amp;headers=false"></iframe>
+        {/* <iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQIZDLRQ29Pjo-sULvwnhdMq6kxcsx7hUHFliu9jg7_v1B5eCd-N6ctuUWfoOrTDS-ijfh75S3HJKmy/pubhtml?gid=1759418424&amp;single=true&amp;widget=true&amp;headers=false"></iframe> */}
       </div>
     </div>
   );
